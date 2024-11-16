5ea1392f0c59782c43a8f24bb186e3b2a6f1bad5
Shivam Chaudhary <shivam.svm007@gmail.com>
2024-11-16 20:16:40 +0100

    Add prisma with mysql db

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,14 @@
-exports.getAllUsers = (req, res) => {
-    res.send('List of all users');
-}
+// userController.js
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
+
+module.exports = { getAllUsers };
