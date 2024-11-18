cec9babd5245e1f0c097008cd1a893d9951762d6
János Euler <janos.euler@gmail.com>
2024-11-18 19:45:43 +0100

    Feat/auth (#3)
    * auth throws errors
    
    * auth working

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 const express = require('express');
 const app = express();
-const usersRouter = require('./routes/users');
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+const { getAllUsers } = require('./controllers/userController');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api/users', usersRouter);
-
-// Default routes
-app.get('/', (req, res) => {
-  res.send('Welcome. Available routes: /api/users');
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+  audience: 'http://localhost:3001/api',
+  issuerBaseURL: `https://dev-boc0av4c0bacnon6.us.auth0.com/`,
 });
 
-app.get('/api', (req, res) => {
-  res.send('Welcome to the API. Available routes: /api/users');
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
 });
+
+const users = require('./models/users.json');
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+app.get('/api/users', checkJwt, function(req, res) {
+  res.json(users)
+});
+
+const checkScopes = requiredScopes('read:messages');
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
