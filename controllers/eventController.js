4c079e9254f37e4487e86cc8b7300e6c4643a9a6
Not22Alan <125280050+Not22Alan@users.noreply.github.com>
2024-11-23 00:54:29 +0100

    fixing the import location

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,4 @@
-const prisma = require('./prisma');
+const prisma = require('../models/prisma');
 
 const getAllEvents = async (req, res) => {
     try {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const prisma = require('./prisma');
+const prisma = require('../models/prisma');
 
 const getAllUsers = async (req, res) => {
   try {
