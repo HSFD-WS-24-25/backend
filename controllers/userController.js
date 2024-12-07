3404d9333d0821d1bb6aa2d8bbe28c8ea50c75f5
Shivam Chaudhary <shivam.svm007@gmail.com>
2024-12-07 19:45:03 +0100

    Add methods to create and check existing user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,43 @@
 const prisma = require('../config/database/prisma');
 
 const getAllUsers = async (req, res) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.json(require('../models/users.json'));
-  } catch (error) {
-    res.status(500).json({ error: 'Something went wrong' });
-  }
+    try {
+        const users = await prisma.user.findMany();
+        res.json(require('../models/users.json'));
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
 };
 
-module.exports = { getAllUsers };
+const createUser = async (sub = null) => {
+    if (!sub) {
+        console.error('No sub provided to createUser');
+    }
+    try {
+        await prisma.user.create({
+            data: {
+                sub: sub,
+            },
+        });
+    } catch (error) {
+        console.error('Error saving user to database:', error);
+    }
+}
+
+const doesUserExist = async (sub = null) => {
+    if (!sub) {
+        console.error('No sub provided to doesUserExist');
+        return false;
+    }
+    try {
+        const countUserExists = await prisma.user.count({
+            where: { sub },
+        });
+        return countUserExists > 0;
+    } catch (error) {
+        console.error('Error checking if user exists:', error);
+        return false;
+    }
+}
+
+module.exports = { getAllUsers, createUser, doesUserExist };
