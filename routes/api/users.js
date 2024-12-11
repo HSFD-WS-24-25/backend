5957321c3b5d5d253d820c9598391f5ebb05a34a
fdai7354 <alan.muhemad@informatik.hs-fulda.de>
2024-12-12 00:41:30 +0100

    changing checkPermissions to checkPermission

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers } = require('../../controllers/userController');
 const logToken  = require('../../middleware/logTokenMiddleware');
-const checkPermissions = require('../../middleware/permissionMiddleware');
+const checkPermission = require('../../middleware/permissionMiddleware');
 const { PERMISSIONS } = require('../../config/permissions');
 
-router.get('/', checkPermissions(PERMISSIONS.VIEW_ALL_USERS), getAllUsers);
+router.get('/', checkPermission(PERMISSIONS.VIEW_ALL_USERS), getAllUsers);
 
 module.exports = router;
