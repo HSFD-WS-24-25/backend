24dc4a6df8868ff36fbb3cc759ebae373600b472
Shivam Chaudhary <shivam.svm007@gmail.com>
2025-01-22 22:02:19 +0100

    Add missing middleware to handle validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,9 @@ const { getAllUsers, updateUser } = require('../../controllers/userController');
 const checkPermission = require('../../middleware/permissionMiddleware');
 const { PERMISSIONS } = require('../../config/permissions');
 const { validateUserData } = require('../../validators/userValidator');
+const handleValidationErrors = require('../../middleware/validationMiddleware');
 
 router.get('/', checkPermission(PERMISSIONS.VIEW_ALL_USERS), getAllUsers);
-router.put('/:id', validateUserData, updateUser);
+router.put('/:id', validateUserData, handleValidationErrors, updateUser);
 
 module.exports = router;
